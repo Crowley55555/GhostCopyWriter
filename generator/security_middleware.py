72636008daa304cb1a9328e119ac058bf7fe2716
Crowley55555 <lavrovartem0511@gmail.com>
2026-02-08 19:22:04 +0300

    Логи: - Убран отладочный вывод (типы, base64, первые 100 символов) в gigachat_api и views

--- generator/security_middleware.py
+++ generator/security_middleware.py
@@ -128,9 +128,9 @@ class SecurityMiddleware(MiddlewareMixin):
             response['Content-Security-Policy'] = (
                 "default-src 'self'; "
                 "script-src 'self' 'unsafe-inline' 'unsafe-eval'; "
-                "style-src 'self' 'unsafe-inline'; "
+                "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "
                 "img-src 'self' data: https:; "
-                "font-src 'self' data:; "
+                "font-src 'self' data: https://fonts.gstatic.com; "
                 "connect-src 'self';"
             )
         
